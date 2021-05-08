3b3bbd37900030f1cef25f3b87ddb9865757ff1d
bryan201-git <bryan_cm96@hotmail.com>
2021-05-08 18:52:29 -0500

    dark mode working

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,16 +1,19 @@
 window.addEventListener('load', () => {
     
+    const body = document.querySelector('body');
     const moon = document.querySelector('.fa-cloud-moon');
     const sun = document.querySelector('.fa-cloud-sun');
 
     moon.addEventListener('click', () => {
         moon.classList.add('hidden');
         sun.classList.remove('hidden');
+        body.classList.toggle('dark');
     });
 
     sun.addEventListener('click', () => {
         sun.classList.add('hidden');
         moon.classList.remove('hidden');
+        body.classList.toggle('dark');
     });
 
 });
