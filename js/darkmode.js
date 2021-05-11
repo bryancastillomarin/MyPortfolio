def72544b7945dfa193635edf9ede396894fb86c
bryan201-git <bryan_cm96@hotmail.com>
2021-05-11 14:15:18 -0500

    saving dark mode in local storage

--- js/darkmode.js
+++ js/darkmode.js
@@ -8,12 +8,18 @@ window.addEventListener('load', () => {
         moon.classList.add('hidden');
         sun.classList.remove('hidden');
         body.classList.toggle('dark');
+        //adding dark mode to local storage
+        localStorage.setItem('dark-mode', 'true');
     });
 
     sun.addEventListener('click', () => {
         sun.classList.add('hidden');
         moon.classList.remove('hidden');
         body.classList.toggle('dark');
+        //removing dark mode from local storage
+        localStorage.removeItem('dark-mode');
     });
 
+    //evaluate local storage
+    localStorage.getItem('dark-mode') && moon.click();
 });
